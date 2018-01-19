1c9580b8c63f972e828f72b1ca5b0f39d5737e19
De Leon Saucedo <luz.de.leon.saucedo@accenture.com>
2018-01-18 16:54:59 -0800

    initial layout

--- clientApp/src/app/app.module.ts
+++ clientApp/src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule,  MatSidenavModule, MatMenuModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { MyHeaderComponent } from './my-header/my-header.component';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyHeaderComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatSidenavModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
